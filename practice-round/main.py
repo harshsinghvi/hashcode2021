8d1f32ae43fc8f3fd65b2db29e5f512e2d7cfb9e
devashishupadhyay <me@thedevashish.in>
2021-01-30 23:19:52 +0000

    test

--- practice-round/main.py
+++ practice-round/main.py
@@ -2,5 +2,3 @@ from random import *
 num = [1,2,1]
 pizza = [['onion','pepper','olive'],['mushroom','tomato','basil'],['chicken','mushroom','pepper'],['tomato','mushroom','basil'],['chicken','basil']]
 ing = 7
-
-t3=[[['o','p'],'ds'] , [ ] ]
